de73d79958c28fff66be4c127ec101657dcdec6f
Maxim Shurygin <maximys2142@mail.ru>
2023-08-30 12:07:41 +0400

    added models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    creation_date = models.DateField(verbose_name='дата создания', **NULLABLE)
+    change_date = models.DateField(verbose_name='дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}: {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
