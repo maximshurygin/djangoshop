bfe7a87326372cc30e5bd564622da6324a1e5b63
Maxim Shurygin <maximys2142@mail.ru>
2023-08-30 19:30:21 +0400

    some fixes

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -9,30 +9,15 @@ class Command(BaseCommand):
         """
         Удаляет данные из базы данных и заполняет их
         """
-        Category.objects.all().delete()
+
         Product.objects.all().delete()
 
-        category_data = [
-            {'name': 'Видеокарты', 'description': 'Различные видеокарты для ПК'},
-            {'name': 'Процессоры', 'description': 'Различные процессоры для ПК'},
-            {'name': 'Блоки питания', 'description': 'Различные блоки питания для ПК'},
-            {'name': 'Материнские платы', 'description': 'Различные материнские платы для ПК'}
-        ]
-
         product_data = [
-            {'name': 'RTX4070', 'category': 'Видеокарты', 'price': '70000'},
-            {'name': 'i9-12900F', 'category': 'Процессоры', 'price': '38000'},
-            {'name': 'DEEPCOOL PQ850M', 'category': 'Блоки питания', 'price': '12500'},
-            {'name': 'GIGABYTE B550', 'category': 'Материнские платы', 'price': '13500'},
+            {'name': 'RTX4070', 'category': Category.objects.get(id=4), 'price': 70000},
+            {'name': 'i9-12900F', 'category': Category.objects.get(id=5), 'price': 38000},
+            {'name': 'DEEPCOOL PQ850M', 'category': Category.objects.get(id=6), 'price': 12500},
+            {'name': 'GIGABYTE B550', 'category': Category.objects.get(id=7), 'price': 13500},
         ]
-
-        category_to_create = []
-
-        for category in category_data:
-            category_to_create.append(
-                Category(**category)
-            )
-        Category.objects.bulk_create(category_to_create)
 
         products_to_create = []
 
--- catalog/models.py
+++ catalog/models.py
@@ -5,24 +5,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование')
-    description = models.TextField(verbose_name='описание', **NULLABLE)
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
-    category = models.CharField(max_length=100, verbose_name='категория')
-    price = models.IntegerField(verbose_name='цена')
-    creation_date = models.DateField(verbose_name='дата создания', **NULLABLE)
-    change_date = models.DateField(verbose_name='дата изменения', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.name}: {self.price}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-        ordering = ('name',)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(verbose_name='описание', **NULLABLE)
@@ -33,4 +15,22 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='id категории')
+    price = models.IntegerField(verbose_name='цена')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания', **NULLABLE)
+    change_date = models.DateTimeField(auto_now=True, verbose_name='дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}: {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
         ordering = ('name',)
